653073f794b6cc076652f7a17baef164c3f70695
Kirill Zaidulin <zaidulinkirill@gmail.com>
2023-01-29 23:55:27 +0300

    initial

---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+import react from '@vitejs/plugin-react'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import dts from 'vite-plugin-dts'
+import { ViteAliases } from 'vite-aliases'
+import { defineConfig } from 'vite'
+
+
+const path = require('path')
+
+export default defineConfig({
+  build: {
+    target: 'esnext',
+    minify: true,
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.tsx'),
+      name: 'mui-admin',
+      fileName: format => `mui-admin.${format}.js`
+    },
+    rollupOptions: {
+      output: {
+        sourcemapExcludeSources: true,
+        globals: {
+          react: 'React',
+          'react/jsx-runtime': 'jsxRuntime',
+          '@mui/material/Box': 'Box',
+          '@mui/material/TextField': 'TextField',
+          '@mui/material/styles': 'styles',
+        }
+      }
+    }
+  },
+  plugins: [
+    peerDepsExternal(),
+    dts({
+      exclude: ['src/components/**/*'],
+      insertTypesEntry: true
+    }),
+    react()
+  ]
+})
