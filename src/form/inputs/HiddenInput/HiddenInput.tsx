ec86dc2027ee22598c8e8039f4a89656eb27a0c7
Kirill Zaidulin <zaidulinkirill@gmail.com>
2023-11-16 15:00:21 +0300

    bump

--- src/form/inputs/HiddenInput/HiddenInput.tsx
+++ src/form/inputs/HiddenInput/HiddenInput.tsx
@@ -1,11 +1,24 @@
+import { FormFetcherHasuraFieldResolver } from 'form/contexts/FormFetcherContext';
+import { FormSubmitterValueResolver } from 'form/contexts/FormSubmitterContext';
+
 import { BaseInput } from '../../core/BaseInput';
 
-export default function HiddenInput({ name }: { name: string }): JSX.Element {
+export default function HiddenInput({
+  name,
+  formFetcherValueResolver,
+  formSubmitterValueResolver,
+}: {
+  name: string;
+  formFetcherValueResolver?: FormFetcherHasuraFieldResolver;
+  formSubmitterValueResolver?: FormSubmitterValueResolver;
+}): JSX.Element {
   return (
     <BaseInput
       name={name}
       skeleton={false}
       grid={false}
+      formFetcherValueResolver={formFetcherValueResolver}
+      formSubmitterValueResolver={formSubmitterValueResolver}
       render={() => null as any}
     />
   );
