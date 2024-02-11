a17285c3ae103d19f8319f43c7c83fc4b327eef0
Kirill Zaidulin <zaidulinkirill@gmail.com>
2024-02-11 17:19:03 +0300

    fixes

--- src/form/utils/getFileIcon.tsx
+++ src/form/utils/getFileIcon.tsx
@@ -42,15 +42,43 @@ export function getFileIcon(
         if (file?.contentType.startsWith('video/')) {
           return (
             <Box
-              component="video"
-              src={`${file.baseUrl}/files/${file.id}#t=0.1`}
               sx={{
                 width: '100%',
                 height: '100%',
-                objectFit: 'cover',
-                borderRadius: '4px',
+                position: 'relative',
               }}
-            />
+            >
+              <Box
+                component="video"
+                src={`${file.baseUrl}/files/${file.id}#t=0.1`}
+                sx={{
+                  width: '100%',
+                  height: '100%',
+                  objectFit: 'cover',
+                  borderRadius: '4px',
+                }}
+              />
+              <Box
+                sx={{
+                  pointerEvents: 'none',
+                  position: 'absolute',
+                  bottom: 0,
+                  right: 0,
+                  top: 0,
+                  left: 0,
+                  margin: '13%',
+                  opacity: 0.9,
+                  color: 'white',
+                }}
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <path
+                    d="M8,5.14V19.14L19,12.14L8,5.14Z"
+                    fill="currentColor"
+                  />
+                </svg>
+              </Box>
+            </Box>
           );
         }
 
